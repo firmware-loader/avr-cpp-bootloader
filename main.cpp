80f2dc945548a258cdaff5d9cd7de3d1fce599cc
Unknown <19Sebastian95@gmx.de>
2019-05-12 22:51:23 +0200

    sync test

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 #include "src/hal/avr/mcus/mega328/Mega328.h"
 #include "src/hal/avr/pin/Control.h"
 #include "src/abstraction/uart/AbstractUart.h"
-#include "src/hal/avr/utils/Timer.h"
 #include "src/abstraction/pins/PinControl.h"
+#include "src/software/uart/SoftwareUart.h"
 
 
 using mcu = lib::avr::ATMega328;
@@ -16,21 +16,17 @@ using mcu = lib::avr::ATMega328;
 int main() {
     using namespace lib::software::literals;
     using uart = lib::software::Uart<mcu>;
+    using softUart = SoftwareUart<mcu>;
 
-    pin::setDirection<pin::Pin<mcu, 8>, pin::Direction::OUTPUT>();
-
-    pin::setDirection<pin::Pin<mcu, 6>, pin::Direction::INPUT>();
-    pin::setInputState<pin::Pin<mcu, 6>, pin::InputState::PULLUP>();
     uart::init<9600_baud>();
-    uart::sendData("GO!\n\r");
+    softUart::init<0>();
 
     while(true) {
-        pin::flipOutputState<pin::Pin<mcu, 8>>();
-        if(pin::readPinState<pin::Pin<mcu, 6>>() == pin::State::ON) {
-            uart::sendData("ON!\n\r");
-        } else {
-            uart::sendData("OFF!\n\r");
+        auto sync = softUart::checkSlot();
+        if(sync == Sync::SYNCED) {
+            uart::sendData("synced!");
+        } else if(sync == Sync::OFFSYNC) {
+            uart::sendData("off!");
         }
-        _delay_ms(500);
     }
 }
--- src/software/uart/SoftwareUart.h
+++ src/software/uart/SoftwareUart.h
@@ -4,12 +4,89 @@
 
 #pragma once
 
-template<typename MicroController>
-class SoftwareUart {
-public:
-    constexpr auto preamble = 0x55;
-    constexpr auto blockstart = 0xCC;
+#include <avr/io.h>
+#include "../../abstraction/pins/PinControl.h"
+
+enum class Sync {
+    SYNCING,
+    SYNCED,
+    OFFSYNC
 };
 
+template<typename mcu>
+class SoftwareUart {
+private:
+    static constexpr auto preamble = 0x55;
+    static constexpr auto blockstart = 0xCC;
+    static constexpr auto measurements = 2;
 
+    static int16_t timer[2];
+    static uint8_t measurement;
+    static bool started;
+    static bool waitForLow;
+    static bool ignoreNextLow;
+
+    static constexpr auto isHigh() {
+        return pin::readPinState<pin::Pin<mcu, 0>>() == pin::State::ON;
+    }
+public:
+    template<auto pinNumber>
+    static constexpr void init() {
+
+        pin::setDirection<pin::Pin<mcu, pinNumber>, pin::Direction::INPUT>();
+        pin::setInputState<pin::Pin<mcu, pinNumber>, pin::InputState::PULLUP>();
+    }
+
+    static constexpr auto checkSlot() {
+        auto high = isHigh();
+
+        if(high && !started) {
+            started = true;
+            waitForLow = true;
+        }
+
+        if(!high && started && !ignoreNextLow) {
+            waitForLow = false;
+            timer[measurement]++;
+        }
+
+        if(!high && ignoreNextLow) {
+            started = false;
+            ignoreNextLow = false;
+        }
+
+        if(high && started && !waitForLow) {
+            started = false;
+            if(measurement + 1 < measurements) {
+                measurement++;
+            } else {
+                waitForLow = false;
+                measurement = 0;
+
+                if(timer[0] - timer[1] < 0) {
+                    ignoreNextLow = true;
+                    return Sync::OFFSYNC;
+                } else {
+                    return Sync::SYNCED;
+                }
+            }
+        }
+        return Sync::SYNCING;
+    }
+};
+
+template<typename mcu>
+int16_t SoftwareUart<mcu>::timer[2] = {0, 0};
+
+template<typename mcu>
+uint8_t SoftwareUart<mcu>::measurement = 0;
+
+template<typename mcu>
+bool SoftwareUart<mcu>::started = false;
+
+template<typename mcu>
+bool SoftwareUart<mcu>::waitForLow = false;
+
+template<typename mcu>
+bool SoftwareUart<mcu>::ignoreNextLow = false;
 
