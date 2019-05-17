50c48b4ac3b530713e66ff0d5ea38e33465342d9
Unknown <19Sebastian95@gmx.de>
2019-05-17 22:07:55 +0200

    bootloader hal concept

--- src/hal/avr/utils/bootloader/mega/Boot.h
+++ src/hal/avr/utils/bootloader/mega/Boot.h
@@ -5,9 +5,17 @@
 #pragma once
 
 namespace lib::avr::boot {
+    <template typename mcu>
     struct BootloaderHal {
     private:
         static constexpr void (*start)() = 0x0000;
+    public:
+        static constexpr writeToFlash(mcu::mem_width data) {
+
+        }
+        static constexpr flushData() {
+
+        }
     };
 }
 
