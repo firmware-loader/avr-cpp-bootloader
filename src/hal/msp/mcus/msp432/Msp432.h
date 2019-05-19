74b2af7ec74689eb3bf429834ca5a389f65624a3
Unknown <19Sebastian95@gmx.de>
2019-05-20 00:38:00 +0200

    msp432

--- src/hal/msp/mcus/msp432/Msp432.h
+++ src/hal/msp/mcus/msp432/Msp432.h
@@ -5,9 +5,30 @@
 #pragma once
 
 
-class msp432 {
+#include "../../../MCUFamilies.h"
+#include "../../../Register.h"
 
-};
+namespace lib {
+    namespace msp {
+        using namespace lib::mcu;
 
+        struct MSP432P401 final {
+            static constexpr MCUFamilies family = MCUFamilies::MSP;
+            using mem_width = uint32_t;
+
+            MSP432P401() = delete;
+
+            struct Port final {
+                Port() = delete;
+
+                DataRegister <Port, ReadOnly, std::byte> in;
+                DataRegister <Port, ReadWrite, std::byte> ddr;
+                DataRegister <Port, ReadWrite, std::byte> out;
+                template<typename L>
+                struct address;
+            };
+        };
+    }
+}
 
 
