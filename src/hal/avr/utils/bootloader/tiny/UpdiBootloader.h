a5152f9db9db5fe42e36a8e3a20db92a88e8204f
Unknown <19Sebastian95@gmx.de>
2019-08-06 18:00:03 +0200

    boot

--- src/hal/avr/utils/bootloader/tiny/UpdiBootloader.h
+++ src/hal/avr/utils/bootloader/tiny/UpdiBootloader.h
@@ -5,6 +5,9 @@
 #pragma once
 #include <avr/io.h>
 #include <avr/pgmspace.h>
+#include <avr/boot.h>
+
+#define SPMControllRegister SPMCSR
 
 namespace lib::avr::boot {
     enum class ResetReason {
@@ -34,20 +37,20 @@ namespace lib::avr::boot {
         }
 
         static bool flashBusy() {
-            return false;
+            return boot_spm_busy();
         }
 
         static void writeFlashPage(uint16_t page) {
-            //boot_page_write(page);
+            boot_page_write(page);
         }
 
         static void clearFlashPage(uint16_t page) {
-            //boot_page_erase(page);
-            //boot_spm_busy_wait();
+            boot_page_erase ((uint32_t) (page));
+            boot_spm_busy_wait ();
         }
 
         static void fillFlashPage(uint16_t addr, uint16_t content) {
-            //boot_page_fill(addr, content);
+            boot_page_fill(addr, content);
         }
 
         static void enableFlash() {
