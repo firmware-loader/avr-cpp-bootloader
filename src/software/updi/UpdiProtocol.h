06b69322680d6dab133f5637bba2348bb97788c2
Unknown <19Sebastian95@gmx.de>
2019-08-13 16:52:41 +0200

    cleanup

--- src/software/updi/UpdiProtocol.h
+++ src/software/updi/UpdiProtocol.h
@@ -50,35 +50,35 @@ namespace lib::software {
 
         static void updiStateMachine(uint8_t data) {
             switch (data & 0xF0) {
-                case UPDI_STCS: { //ignore next
+                case updi::UPDI_STCS: {
                     uint8_t value = softUPDI::getByteWithoutSync();
                     switch (data & 0x0F) {
-                        case UPDI_ASI_RESET_REQ:
-                            if (value == UPDI_RESET_REQ_VALUE) {
+                        case updi::UPDI_ASI_RESET_REQ:
+                            if (value == updi::UPDI_RESET_REQ_VALUE) {
                                 start_pgm();
                             }
                     }
                     break;
                 }
-                case UPDI_LDCS:
+                case updi::UPDI_LDCS:
                     switch (data & 0x0F) {
-                        case UPDI_CS_STATUSA:
+                        case updi::UPDI_CS_STATUSA:
                             softUPDI::sendChar(0x30); // is this ack?
                             break;
-                        case UPDI_ASI_SYS_STATUS:
+                        case updi::UPDI_ASI_SYS_STATUS:
                             sendStatusInfo();
                             break;
                     }
                     break;
-                case UPDI_KEY:
+                case updi::UPDI_KEY:
                     switch (data & 0x0F) {
-                        case UPDI_KEY_SIB | UPDI_SIB_16BYTES:
-                            softUPDI::sendString(DEVICE_STRING);
+                        case updi::UPDI_KEY_SIB | updi::UPDI_SIB_16BYTES:
+                            softUPDI::sendString(updi::DEVICE_STRING);
                     }
                     break;
-                case UPDI_ST:
-                    if (data & UPDI_PTR_ADDRESS) {
-                        if (data & UPDI_DATA_16) {
+                case updi::UPDI_ST:
+                    if (data & updi::UPDI_PTR_ADDRESS) {
+                        if (data & updi::UPDI_DATA_16) {
                             //16 Bit Data
                             store.ptr = getWordValue();
                             store.repeats = 1;
@@ -86,33 +86,33 @@ namespace lib::software {
                             // 8 Bit
                             store.ptr = softUPDI::getByte();
                         }
-                        softUPDI::sendChar(UPDI_PHY_ACK);
-                    } else if (data & UPDI_PTR_INC) {
-                        if (data & UPDI_DATA_16) {
-                            if (store.ptr >= UPDI_ADDRESS_OFFSET) {
+                        softUPDI::sendChar(updi::UPDI_PHY_ACK);
+                    } else if (data & updi::UPDI_PTR_INC) {
+                        if (data & updi::UPDI_DATA_16) {
+                            if (store.ptr >= updi::UPDI_ADDRESS_OFFSET) {
                                 writeToFlashBuffer();
                             }
                         }
                     }
                     break;
-                case UPDI_STS: {
-                    if (data & UPDI_ADDRESS_16) {
+                case updi::UPDI_STS: {
+                    if (data & updi::UPDI_ADDRESS_16) {
                         getWordValue();
                     }
-                    softUPDI::sendChar(UPDI_PHY_ACK);
+                    softUPDI::sendChar(updi::UPDI_PHY_ACK);
                     executeSTSControlCommand();
                     break;
                 }
-                case UPDI_LD:
-                    if (data & UPDI_PTR_INC) {
-                        if (!(data & UPDI_DATA_16)) {
+                case updi::UPDI_LD:
+                    if (data & updi::UPDI_PTR_INC) {
+                        if (!(data & updi::UPDI_DATA_16)) {
                             auto ptr_data = lookup_ptr();
                             for (uint8_t i = 0; i < store.repeats; i++) {
                                 softUPDI::sendChar(ptr_data[i]);
                             }
                         } else {
-                            if (store.ptr >= UPDI_ADDRESS_OFFSET) {
-                                uint16_t start_address = store.ptr - UPDI_ADDRESS_OFFSET;
+                            if (store.ptr >= updi::UPDI_ADDRESS_OFFSET) {
+                                uint16_t start_address = store.ptr - updi::UPDI_ADDRESS_OFFSET;
                                 for (uint8_t i = 0; i < store.repeats * 2; i++) {
                                     softUPDI::sendChar(bootloader::readFlash(start_address + i));
                                 }
@@ -120,18 +120,18 @@ namespace lib::software {
                         }
                     }
                     break;
-                case UPDI_LDS:
-                    if (data & UPDI_ADDRESS_16) {
+                case updi::UPDI_LDS:
+                    if (data & updi::UPDI_ADDRESS_16) {
                         store.ptr = getWordValue();
                         store.repeats = 1;
-                        if(store.ptr == NVMCTRL_ADDRESS + UPDI_NVMCTRL_STATUS) {
+                        if(store.ptr == updi::NVMCTRL_ADDRESS + updi::UPDI_NVMCTRL_STATUS) {
                             if(store.flash_write_error) {
-                                softUPDI::sendChar(1 << UPDI_NVM_STATUS_WRITE_ERROR);
+                                softUPDI::sendChar(1 << updi::UPDI_NVM_STATUS_WRITE_ERROR);
                             } else {
                                 if(bootloader::flashBusy()) {
-                                    softUPDI::sendChar(1 << UPDI_NVM_STATUS_FLASH_BUSY);
+                                    softUPDI::sendChar(1 << updi::UPDI_NVM_STATUS_FLASH_BUSY);
                                 } else {
-                                    softUPDI::sendChar(UPDI_NVM_STATUS_READY);
+                                    softUPDI::sendChar(updi::UPDI_NVM_STATUS_READY);
                                     bootloader::enableFlash();
                                 }
                             }
@@ -140,8 +140,8 @@ namespace lib::software {
                         }
                     }
                     break;
-                case UPDI_REPEAT:
-                    if (data & UPDI_REPEAT_WORD) {
+                case updi::UPDI_REPEAT:
+                    if (data & updi::UPDI_REPEAT_WORD) {
                         store.repeats = getWordValue() + 1;
                     } else {
                         store.repeats = softUPDI::getByteWithoutSync() + 1u;
@@ -152,15 +152,15 @@ namespace lib::software {
 
         static void executeSTSControlCommand() {
             uint8_t flash_cmd = softUPDI::getByteWithoutSync();
-            if (flash_cmd == UPDI_NVMCTRL_CTRLA_CHIP_ERASE) {
-                //TODO: Erase Chip
-            } else if (flash_cmd == UPDI_NVMCTRL_CTRLA_PAGE_BUFFER_CLR) {
-                //TODO: Clear Page Buffer
-            } else if (flash_cmd == UPDI_NVMCTRL_CTRLA_WRITE_PAGE) {
-                uint16_t startAddress = store.ptr - UPDI_ADDRESS_OFFSET;
+            if (flash_cmd == updi::UPDI_NVMCTRL_CTRLA_CHIP_ERASE) {
+                // Erase Chip
+            } else if (flash_cmd == updi::UPDI_NVMCTRL_CTRLA_PAGE_BUFFER_CLR) {
+                // Clear Page Buffer
+            } else if (flash_cmd == updi::UPDI_NVMCTRL_CTRLA_WRITE_PAGE) {
+                uint16_t startAddress = store.ptr - updi::UPDI_ADDRESS_OFFSET;
                 bootloader::writeFlashPage(startAddress);
             }
-            softUPDI::sendChar(UPDI_PHY_ACK);
+            softUPDI::sendChar(updi::UPDI_PHY_ACK);
         }
 
         static uint16_t getWordValue() {
@@ -170,7 +170,7 @@ namespace lib::software {
         }
 
         static void writeToFlashBuffer() {
-            uint16_t startAddress = store.ptr - UPDI_ADDRESS_OFFSET;
+            uint16_t startAddress = store.ptr - updi::UPDI_ADDRESS_OFFSET;
             bootloader::clearFlashPage(startAddress);
             for (uint8_t i = 0; i < store.repeats * 2; i+=2) {
                 bootloader::fillFlashPage(startAddress + i, getWordValue());
@@ -180,16 +180,16 @@ namespace lib::software {
         static void sendStatusInfo() {
             //TODO: Fully add status Info
             //We are already in prog mode... or at least we say so
-            constexpr uint8_t status = (1 << UPDI_ASI_SYS_STATUS_NVMPROG);
+            constexpr uint8_t status = (1 << updi::UPDI_ASI_SYS_STATUS_NVMPROG);
             softUPDI::sendChar(status);
         }
 
         static constexpr const uint8_t* lookup_ptr() {
             switch(store.ptr) {
                 case 0x1100:
-                    return UPDI_DEVICE_ID;
+                    return updi::UPDI_DEVICE_ID;
                 case 0x0F01:
-                    return UPDI_DEVICE_REVISION;
+                    return updi::UPDI_DEVICE_REVISION;
             }
             return nullptr;
         }
